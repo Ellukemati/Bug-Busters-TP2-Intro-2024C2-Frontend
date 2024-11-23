b976a982ab5353e42ccad0112c5193a43fbcc679
Asimbron <imac@iMac.fibertel.com.ar>
2024-11-23 19:35:56 -0300

    se arreglo el reseteo del filtro y se agrego la hilera de los tipos

--- src/routes/movimientos/+page.svelte
+++ src/routes/movimientos/+page.svelte
@@ -1,27 +1,53 @@
 <script>
     export let data;
+    import Typeahead from "svelte-typeahead";
+    const tipoColores = {
+        "Normal": "#aaaa99",
+        "Fuego": "#ff4422",
+        "Agua": "#3399ff",
+        "Eléctrico": "#ffcc33",
+        "Planta": "#77cc55",
+        "Hielo": "#66ccff",
+        "Lucha": "#bb5544",
+        "Veneno": "#aa5599",
+        "Tierra": "#ddbb55",
+        "Volador": "#8899ff",
+        "Psíquico": "#ff5599",
+        "Bicho": "#aabb22",
+        "Roca": "#bbaa66",
+        "Fantasma": "#6666bb",
+        "Dragón": "#7766ee",
+        "Siniestro": "#775544",
+        "Acero": "#aaaabb",
+        "Hada": "#ee99ee"
+    };
+    let color_tipo = tipoColores.Normal
     const movimientos = data.movimientos
     function myFunction() {
-        let input, table, tr, td, i, txtValue;
-        input = document.getElementById("ingreso");
-        table = document.getElementById("Tabla");
-        tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
-                    tr[i].style.display
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
+        var input, filter, table, tbody, tr, i, td, j, txtValue; 
+  input = document.getElementById("ingreso"); 
+  filter = input.value.toUpperCase(); 
+  table = document.getElementById("Tabla"); 
+  tbody = table.getElementsByTagName("tbody")[0]; 
+  tr = tbody.getElementsByTagName("tr"); 
+ 
+  // Loop through all table rows, then through all row cells, 
+  // and hide the rows where none of the cells match the filter 
+  for (i = 0; i < tr.length; i++) { 
+      tr[i].style.display = "none"; 
+      td = tr[i].getElementsByTagName("td")[1]; 
+        if (td) { 
+          txtValue = td.textContent || td.innerText; 
+          if (txtValue.toUpperCase().indexOf(filter) > -1) { 
+            tr[i].style.display = ""; 
+          } 
+        } 
+      } 
     }
     function resetSearch() {
-        let input = document.getElementById("ingreso");
-        input.value = '';
-        myFunction();
+        var input = document.getElementById("ingreso"); 
+        input.value = ""; 
+        myFunction(); // force the table to reset since there is no keyup action here 
     }
 </script>
 <h1>Movimientos</h1>
@@ -29,22 +55,36 @@
     <label for="filtrar movimientos">Nombre:</label>
     <input type="search" id="ingreso" placeholder="nombre..">
     <label for="ingresar">buscar:</label>
-    <button type="submit" on:click={myFunction()} aria-label="ingresar"></button>
+    <button type="submit" on:click={myFunction()} aria-label="ingresar" >buscar</button>
     <label for="reiniciar">reiniciar:</label>
-    <button type="reset" on:click={resetSearch()} aria-label="reiniciar"></button>
+    <button type="reset" on:click={resetSearch()} aria-label="reiniciar">reiniciar</button>
 </form>
+<Typeahead
+   label="Preseleccionar alumno"
+   placeholder={`Buscar alumno por nombre o apellido`}
+   data={data.movimientos}
+   extract={(movimiento) => `${movimiento.nombre}`}
+   inputAfterSelect='clear'
+/>
+
 <table id="Tabla">
    <thead>
        <tr>
-           <th style="width:40%;">Numero</th>
+           <th style="width:30%;">Numero</th>
            <th style="width:60%;">Nombre</th>
+           <th style="width:10%;">Tipo</th>
        </tr>
    </thead>
    <tbody>
        {#each movimientos as movimiento}
            <tr>
-               <td>{movimiento.id}</td>
-               <td>{movimiento.nombre}</td>
+               <td class="not-tipo">{movimiento.id}</td>
+               <td class="not-tipo">{movimiento.nombre}</td>
+               <td>
+                    <p class="{movimiento.tipo}">
+                        {movimiento.tipo}       
+                    </p>
+                </td>
            </tr>
        {/each}
    </tbody>
@@ -54,7 +94,7 @@
 <style>
     button{
         width: 100%;
-        font-size: 12px;
+        font-size: 16px;
         padding: 12px 12px 12px 40px;
         border: 6px solid #ddddddba;
         margin-bottom: 12px;
@@ -78,12 +118,339 @@
     width: 100%;
     }
 
-    td, th {
+     th {
         border: 10px solid black;
         text-align: left;
         padding: 8px;
     }
-
+    .Hada{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #ee99ee;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Acero{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #aaaabb;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Siniestro{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #775544;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Dragón{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #7766ee;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Fantasma{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #6666bb;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Roca{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #bbaa66;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Bicho{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #aabb22;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Psíquico{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #ff5599;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Volador{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #8899ff;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Tierra{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #ddbb55;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Veneno{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #aa5599;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Lucha{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #bb5544;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Hielo{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #66ccff;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Planta{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #77cc55;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Oscuro{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #56569d;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Eléctrico{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #ffcc33;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Agua{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #3399ff;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Fuego{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #ff4422;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    .Normal{
+        display: inline-block;
+    width: 66px;
+    margin-bottom: 4px;
+    background: #aaaa99;
+    border: 1px solid #a3a3a3;
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    color: #fff;
+    font-size: .75rem;
+    font-weight: normal;
+    line-height: 1.5rem;
+    text-align: center;
+    text-shadow: 1px 1px 2px rgba(0,0,0,.7);
+    text-transform: uppercase;
+    transition: opacity .4s
+    }
+    td{
+        border: 10px solid black;
+        text-align: left;
+        padding: 8px;
+    }
     tr:nth-child(even) {
         background-color: #33ecff;
         }
