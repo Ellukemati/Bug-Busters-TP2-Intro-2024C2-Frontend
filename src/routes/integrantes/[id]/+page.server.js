6bbab026bb8ec9dfc5bc1443a7e57256897ae083
Asimbron <imac@iMac.fibertel.com.ar>
2024-11-29 15:37:20 -0300

    se creo la pagina para integrantes particulares

---
+++ src/routes/integrantes/[id]/+page.server.js
@@ -0,0 +1,29 @@
+import { error } from '@sveltejs/kit';
+
+export async function load( {params} ) {
+    let url = new URL('http://localhost:8000/teams/')
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+ 
+    let equipos = await response.json();
+    let integrante = await encontrar_integrante(params.id, equipos)
+    return {
+        integrante: integrante
+    };
+ }
+async function encontrar_integrante(id, equipos) {
+    let integrante
+    for (let i = 0; i < equipos.length; i++){
+        for (let j = 0; j < equipos[i].pokemons_de_equipo.length; j++){
+            if (equipos[i].pokemons_de_equipo[j].id == id){
+                integrante = equipos[i].pokemons_de_equipo[j]
+            }
+        }
+    }
+    if (integrante == null){
+        throw error(404, "el integrante no fue encontrado");
+    }
+    return integrante
+}
---
+++ src/routes/integrantes/[id]/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+    let { data } = $props();
+    const integrante = data.integrante
+    import "./estilos.css"
+ </script>
+<h1 class="titulo_integrante">Integrante</h1>
+<div class="contenido_integrante">
+    <div class="id">
+        <p class="texto">id del integrante: {integrante.id}</p>
+    </div>
+    <div>
+        <a href="{`/pokemons/${integrante.pokemon_id}`}">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{integrante.pokemon_id}.png" alt="imagen del integrante" class="imagen_integrante">
+        </a>
+    </div>
+    <div>
+        <p class = "texto">naturaleza: {integrante.naturaleza}</p>
+    </div>
+</div>
+<h2>Movimientos que utiliza</h2>
+<table class="tabla_movs_usados">
+    <thead>
+        <tr>
+            <th style="width:20%;" class="titulo_hilera">Numero</th>
+            <th style="width:50%;" class="titulo_hilera">Nombre</th>
+            <th style="width:10%;" class="titulo_hilera">Tipo</th>
+            <th style="width:10%;" class="titulo_hilera">Categoria</th>
+            <th style="width:10%;" class="titulo_hilera">Potencia</th>
+        </tr>
+    </thead>
+    <tbody class="cuerpo_tabla">
+        {#each integrante.movimientos as movimiento}
+            <tr>
+                <td class="contenido_hilera">
+                    <p class="texto">
+                        {movimiento.id}
+                    </p>
+                </td>
+                <td class="contenido_hilera">
+                 <a href="/movimientos/{movimiento.id}">
+                    <p class="texto">
+                        {movimiento.nombre}
+                    </p>
+                 </a>
+             </td>
+                <td class="contenido_hilera">
+                     <p class="texto">
+                         {movimiento.tipo}       
+                     </p>
+                 </td>
+                <td class="contenido_hilera">
+                     <p class="texto">
+                         {movimiento.categoria}
+                     </p>
+                </td>
+                <td class="contenido_hilera">
+                    <p class="texto">
+                        {movimiento.power}
+                    </p>
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
